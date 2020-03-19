5c8fe7a14d96432d92421d4baba1610e6a04e565
Patrick <okhofsk@gmail.com>
2020-03-19 19:29:55 +0100

    more stuff

--- matrix_games.py
+++ matrix_games.py
@@ -1,16 +1,20 @@
 # Import Libraries and Dependencies
 import numpy as np
 import matplotlib.pyplot as plt
-from random import randrange
+from random import uniform
 from time import perf_counter
 import scipy as sp
 from scipy.sparse import random
 import numpy.linalg as LA
 
-# create a random A matrix that is either sparse or non-sparse
+## --------------------------------------------------------------------------------##
+                            ## External Functions ##
+## --------------------------------------------------------------------------------##
+
+
 def create_A(n, m, sparsity):
     """
-    Internal function for matrix games library'
+    External function for matrix games library'
     
     Computes a matrix with random entries
     
@@ -21,90 +25,27 @@ def create_A(n, m, sparsity):
     m : int
         number of rows.
     sparsity : boolean
-        if "True" it returns a sparse matrix using the scipy.sparse
+        if "True" it returns a random sparse matrix using the scipy.sparse
         library[1], otherwise it computes a full matrix with random
-        entries from 0 to 
+        entries from 0 to 1. 
         
     Returns
     -------
-    out : complex ndarray
-        The truncated or zero-padded input, transformed along the axis
-        indicated by `axis`, or the last one if `axis` is not specified.
+    out : ndarray or a sparse matrix in csr format.[2]
+    
     Raises
     ------
-    IndexError
-        if `axes` is larger than the last axis of `x`.
-    See Also
-    --------
-    ifft : The inverse of `fft`.
-    fft2 : The 2-D FFT.
-    fftn : The N-D FFT.
-    rfftn : The N-D FFT of real input.
-    fftfreq : Frequency bins for given FFT parameters.
-    next_fast_len : Size to pad input to for most efficient transforms
+        
     Notes
     -----
-    FFT (Fast Fourier Transform) refers to a way the discrete Fourier Transform
-    (DFT) can be calculated efficiently, by using symmetries in the calculated
-    terms. The symmetry is highest when `n` is a power of 2, and the transform
-    is therefore most efficient for these sizes. For poorly factorizable sizes,
-    `scipy.fft` uses Bluestein's algorithm [2]_ and so is never worse than
-    O(`n` log `n`). Further performance improvements may be seen by zero-padding
-    the input using `next_fast_len`.
-    If ``x`` is a 1d array, then the `fft` is equivalent to ::
-        y[k] = np.sum(x * np.exp(-2j * np.pi * k * np.arange(n)/n))
-    The frequency term ``f=k/n`` is found at ``y[k]``. At ``y[n/2]`` we reach
-    the Nyquist frequency and wrap around to the negative-frequency terms. So,
-    for an 8-point transform, the frequencies of the result are
-    [0, 1, 2, 3, -4, -3, -2, -1]. To rearrange the fft output so that the
-    zero-frequency component is centered, like [-4, -3, -2, -1, 0, 1, 2, 3],
-    use `fftshift`.
-    Transforms can be done in single, double, or extended precision (long
-    double) floating point. Half precision inputs will be converted to single
-    precision and non-floating-point inputs will be converted to double
-    precision.
-    If the data type of ``x`` is real, a "real FFT" algorithm is automatically
-    used, which roughly halves the computation time. To increase efficiency
-    a little further, use `rfft`, which does the same calculation, but only
-    outputs half of the symmetrical spectrum. If the data are both real and
-    symmetrical, the `dct` can again double the efficiency, by generating
-    half of the spectrum from half of the signal.
-    When ``overwrite_x=True`` is specified, the memory referenced by ``x`` may
-    be used by the implementation in any way. This may include reusing the
-    memory for the result, but this is in no way guaranteed. You should not
-    rely on the contents of ``x`` after the transform as this may change in
-    future without warning.
-    The ``workers`` argument specifies the maximum number of parallel jobs to
-    split the FFT computation into. This will execute independent 1-D
-    FFTs within ``x``. So, ``x`` must be at least 2-D and the
-    non-transformed axes must be large enough to split into chunks. If ``x`` is
-    too small, fewer jobs may be used than requested.
+    
     References
     ----------
-    .. [1] Cooley, James W., and John W. Tukey, 1965, "An algorithm for the
-           machine calculation of complex Fourier series," *Math. Comput.*
-           19: 297-301.
-    .. [2] Bluestein, L., 1970, "A linear filtering approach to the
-           computation of discrete Fourier transform". *IEEE Transactions on
-           Audio and Electroacoustics.* 18 (4): 451-455.
+    .. [1] https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.random.html#scipy.sparse.random
+    .. [2] https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html#scipy.sparse.csr_matrix
+    
     Examples
     --------
-    >>> import scipy.fft
-    >>> scipy.fft.fft(np.exp(2j * np.pi * np.arange(8) / 8))
-    array([-2.33486982e-16+1.14423775e-17j,  8.00000000e+00-1.25557246e-15j,
-            2.33486982e-16+2.33486982e-16j,  0.00000000e+00+1.22464680e-16j,
-           -1.14423775e-17+2.33486982e-16j,  0.00000000e+00+5.20784380e-16j,
-            1.14423775e-17+1.14423775e-17j,  0.00000000e+00+1.22464680e-16j])
-    In this example, real input has an FFT which is Hermitian, i.e., symmetric
-    in the real part and anti-symmetric in the imaginary part:
-    >>> from scipy.fft import fft, fftfreq, fftshift
-    >>> import matplotlib.pyplot as plt
-    >>> t = np.arange(256)
-    >>> sp = fftshift(fft(np.sin(t)))
-    >>> freq = fftshift(fftfreq(t.shape[-1]))
-    >>> plt.plot(freq, sp.real, freq, sp.imag)
-    [<matplotlib.lines.Line2D object at 0x...>, <matplotlib.lines.Line2D object at 0x...>]
-    >>> plt.show()
     """
     if sparsity:
         return sp.sparse.random(n, m)
@@ -112,21 +53,227 @@ def create_A(n, m, sparsity):
         a = np.zeros([n,m])
         for i in range(n):
             for j in range(m):
-                a[i,j] = randrange(10)
+                a[i,j] = uniform(-1,1)
         return a
-    
-# create the F operator, only needed if wanted explicitly
+
+## --------------------------------------------------------------------------------##
+
 def create_F(a):
-    zero_matrix_top = np.zeros((a.shape[1], a.shape[1]))
-    zero_matrix_bot = np.zeros((a.shape[0], a.shape[0]))
-    left_F = np.concatenate((zero_matrix_top, -a))
-    right_F = np.concatenate((a.transpose(), zero_matrix_bot))
+    """
+    External function for matrix games library'
+    
+    Computes the F operator explicitly for the matrix games problem which is
+    given in a matrix form by F = [[0, A^T];[-A, 0]] where 0 indicates a zero
+    matrix of the appropriate size (for A a nxm matrix we have a mxm matrix 
+    in the top left corner and a nxn matrix on the bottom right corner.
+    
+    Parameters
+    ----------
+    a : ndarray/sparse.csr[1]
+        the matrix A from the matrix games problem in sparse or ndarray form
+        
+    Returns
+    -------
+    out : ndarray 
+        the operator F in matrix form as given by theory
+    
+    Raises
+    ------
+        
+    Notes
+    -----
+    
+    References
+    ----------
+    .. [1] https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.random.html#scipy.sparse.random
+    
+    Examples
+    --------
+    """
+    if sp.sparse.issparse(a):
+        mat_a = np.copy(a.toarray())
+    else:
+        mat_a = np.copy(a)
+    zero_matrix_top = np.zeros((mat_a.shape[1], mat_a.shape[1]))
+    zero_matrix_bot = np.zeros((mat_a.shape[0], mat_a.shape[0]))
+    left_F = np.concatenate((zero_matrix_top, -mat_a))
+    right_F = np.concatenate((mat_a.transpose(), zero_matrix_bot))
     return np.concatenate((left_F, right_F), axis=1)
 
-# multiplies the input vector with the F operator
-def Fx_operator(a, x):
+## --------------------------------------------------------------------------------##
+
+def create_F_rand(n, m, sparsity):
+    """
+    External function for matrix games library'
+    
+    Computes the F operator explicitly for the matrix games problem using a matrix A
+    with random variables of size nxm which is given in a matrix form by 
+    F = [[0, A^T];[-A, 0]] where 0 indicates a zero matrix of the appropriate size 
+    (for A a nxm matrix we have a mxm matrix in the top left corner and a nxn matrix 
+    on the bottom right corner.
+    
+    Parameters
+    ----------
+    n : int
+        number of columns.
+    m : int
+        number of rows.
+    sparsity : boolean
+        if "True" it returns a random sparse matrix using the scipy.sparse
+        library[1], otherwise it computes a full matrix with random
+        entries from 0 to 1. 
+        
+    Returns
+    -------
+    out : ndarray 
+        the operator F in matrix form as given by theory
+    
+    Raises
+    ------
+        
+    Notes
+    -----
+    
+    References
+    ----------
+    .. [1] https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.random.html#scipy.sparse.random
+    .. [2] https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html#scipy.sparse.csr_matrix
+    
+    Examples
+    --------
+    """
+    if sparsity:
+        mat_a = create_A(n,m,sparsity).toarray()
+    else:
+        mat_a = create_A(n,m,sparsity)
+    zero_matrix_top = np.zeros((mat_a.shape[1], mat_a.shape[1]))
+    zero_matrix_bot = np.zeros((mat_a.shape[0], mat_a.shape[0]))
+    left_F = np.concatenate((zero_matrix_top, -mat_a))
+    right_F = np.concatenate((mat_a.transpose(), zero_matrix_bot))
+    return np.concatenate((left_F, right_F), axis=1)
+
+## --------------------------------------------------------------------------------##
+
+def Fx_product(a, x):
+    """
+    External function for matrix games library'
+    
+    Computes the Fx product using the matrix a and the vector x. By using the nature of the 
+    F matrix we can make the operation more efficient since we only have to multiply the a
+    matrix part with part of the vector x and not the whole F matrix. 
+    
+    Parameters
+    ----------
+    a : ndarray/sparse.csr[1]
+        the matrix A from the matrix games problem in sparse or ndarray form
+    x : ndarray
+        the vector x 
+        
+    Returns
+    -------
+    out : ndarray 
+        the product Fx as a vector
+    
+    Raises
+    ------
+        
+    Notes
+    -----
+    
+    References
+    ----------
+    .. [1] https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html#scipy.sparse.csr_matrix
+    
+    Examples
+    --------
+    """
+    if sp.sparse.issparse(a):
+        _A = a.toarray()
+    else :
+        _A = a
+        
     x_ = np.reshape(x, (len(x),1))
-    (dimN, dimM) = a.shape
-    x_top = a.T.dot(x_[dimM-1:dimN+dimM-1])
-    x_bot = -a.dot(x_[:dimM])
+    (dimN, dimM) = _A.shape
+    x_top = _A.T.dot(x_[dimM-1:dimN+dimM-1])
+    x_bot = -_A.dot(x_[:dimM])
     return np.append(x_top, x_bot)
+
+## --------------------------------------------------------------------------------##
+
+def Fx(a):
+    """
+    External function for matrix games library'
+    
+    Computes the Fx operator using the matrix a. By using the function "Fx_product".
+    
+    Parameters
+    ----------
+    a : ndarray/sparse.csr[1]
+        the matrix A from the matrix games problem in sparse or ndarray form
+        
+    Returns
+    -------
+    out : function 
+        a function that descibes the Fx operator 
+    
+    Raises
+    ------
+        
+    Notes
+    -----
+    
+    References
+    ----------
+    .. [1] https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html#scipy.sparse.csr_matrix
+    
+    Examples
+    --------
+    """
+    def F(q):
+        return Fx_product(a, q)
+    return F
+
+## --------------------------------------------------------------------------------##
+
+def J_operator(a): 
+    """
+    External function for matrix games library'
+    
+    Computes the J operator
+    
+    Parameters
+    ----------
+    a : ndarray/sparse.csr[1]
+        the matrix A from the matrix games problem in sparse or ndarray form
+        
+    Returns
+    -------
+    out : function 
+        a function that descibes the J operator 
+    
+    Raises
+    ------
+        
+    Notes
+    -----
+    
+    References
+    ----------
+    .. [1] https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html#scipy.sparse.csr_matrix
+    
+    Examples
+    --------
+    """
+    if sp.sparse.issparse(a):
+        _A = a.toarray()
+    else :
+        _A = a
+    
+    (dimN, dimM) = _A.shape;
+    
+    def J(q):
+        Fx = Fx_product(_A, q)
+        Ax = Fx[:dimM]
+        ATx = Fx[dimM-1:dimN+dimM-1]
+        return np.amax(Ax) - np.amin(ATx)
+    return J
